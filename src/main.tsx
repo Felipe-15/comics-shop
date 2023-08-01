8d0f96adc0ac451ff11f7068eb987ff96191bd4a
Felipe Souza <felipesouza036@gmail.com>
2023-08-01 11:52:06 -0300

    :sparkles: feat (main): Change to Routers with Data Api

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
 import App from "./App.tsx";
 import GlobalStyles from "./styles/GlobalStyles.ts";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme.ts";
 import Header from "./components/Header/index.tsx";
+import ComicsList from "./templates/Comics/index.tsx";
+import { getComics } from "./services/comics/getComics.ts";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route
+        path="/"
+        loader={async () => {
+          const comics = await getComics();
+
+          return comics;
+        }}
+        element={<ComicsList />}
+      />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalStyles />
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<App />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
