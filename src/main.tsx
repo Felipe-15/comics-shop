6215a251ac35477ac04e50038cf667f04907b52b
Felipe Souza <felipesouza036@gmail.com>
2023-08-01 11:17:14 -0300

    :sparkles: feat (main): Add Browser Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
 import GlobalStyles from "./styles/GlobalStyles.ts";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme.ts";
+import Header from "./components/Header/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalStyles />
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
